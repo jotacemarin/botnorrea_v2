441c434555b8f84ad5fb52e6ad62907d120b69e8
jotacemarin <jotacemarin@gmail.com>
2023-08-16 11:01:33 -0500

    close #26 remove unused functions

--- index.ts
+++ index.ts
@@ -2,6 +2,3 @@ export { authorizer } from "./src/functions/authorizer";
 export { telegramSetWebhook } from "./src/functions/telegram_set_webhook"; 
 export { telegramWebhook } from "./src/functions/telegram_webhook"; 
 export { dynamoDBUsersCrud } from "./src/functions/dynamo_users_crud";
-
-export { handler } from "./src/functions/handler";
-export { dummy } from "./src/functions/dummy";
