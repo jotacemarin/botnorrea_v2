f712f3208ff8b5648a700b81e9cc470d7cab9790
jotacemarin <jotacemarin@gmail.com>
2023-08-29 14:25:29 -0500

    add more unit test to botnorrea commands create api key

--- src/functions/botnorrea_create_api_key/index.test.ts
+++ src/functions/botnorrea_create_api_key/index.test.ts
@@ -1,9 +1,17 @@
-import { OK, FORBIDDEN, NOT_FOUND } from "http-status";
-import { FormattingOptionsTg } from "../../models";
+import {
+  OK,
+  FORBIDDEN,
+  NOT_FOUND,
+  INTERNAL_SERVER_ERROR,
+  UNAUTHORIZED,
+  BAD_REQUEST,
+} from "http-status";
+import { FormattingOptionsTg, Role } from "../../models";
 import usersDynamoService from "../../services/dynamoUsersServices";
 import { sendMessage } from "../../services/telegram";
-import { execute } from "./index";
+import { botnorreaCreateApiKey, execute } from "./index";
 
+const logError = jest.spyOn(console, "error").mockImplementation(() => {});
 jest.mock("../../services/dynamoUsersServices");
 jest.mock("../../services/telegram");
 
@@ -23,6 +31,7 @@ describe("execute", () => {
 
   afterEach(() => {
     jest.clearAllMocks();
+    jest.resetAllMocks();
   });
 
   it("should create API key and send message for a valid user", async () => {
@@ -91,5 +100,222 @@ describe("execute", () => {
       reply_to_message_id: "mockMessageId",
     });
     expect(result).toEqual({ statusCode: FORBIDDEN });
+  });
+
+  it("should handle internal server error", async () => {
+    usersDynamoService.getById.mockResolvedValueOnce(mockUser);
+    usersDynamoService.update.mockRejectedValueOnce(new Error("mock error"));
+
+    const result = await execute(mockTelegramUpdate);
+
+    expect(usersDynamoService.getById).toHaveBeenCalledWith("mockUserId");
+    expect(logError).toHaveBeenCalled();
+    expect(result).toEqual({ statusCode: INTERNAL_SERVER_ERROR });
+  });
+});
+
+describe("botnorreaCreateApiKey", () => {
+  const mockTelegramUpdate = {
+    message: {
+      from: { id: "mockUserId" },
+      chat: { id: "mockChatId", type: "private" },
+      message_id: "mockMessageId",
+    },
+  };
+
+  const mockUser = {
+    uuid: "mockUserUuid",
+    apiKey: "mockApiKey",
+    role: Role.ROOT,
+  };
+
+  afterEach(() => {
+    jest.clearAllMocks();
+    jest.resetAllMocks();
+  });
+
+  it("should create API key and send message for a valid user", async () => {
+    const mockCallback = jest.fn();
+    usersDynamoService.get.mockResolvedValueOnce(mockUser);
+    usersDynamoService.getById.mockResolvedValueOnce({ uuid: "mockUserUuid" });
+
+    await botnorreaCreateApiKey(
+      {
+        queryStringParameters: {
+          id: "mockUserId",
+          apiKey: "mockApiKey",
+        },
+        body: JSON.stringify(mockTelegramUpdate),
+      },
+      {},
+      mockCallback
+    );
+
+    expect(usersDynamoService.get).toHaveBeenCalledWith("mockUserId");
+    expect(usersDynamoService.getById).toHaveBeenCalledWith("mockUserId");
+    expect(usersDynamoService.update).toHaveBeenCalledWith(
+      { uuid: "mockUserUuid", apiKey: expect.any(String) },
+      true
+    );
+    expect(sendMessage).toHaveBeenCalledWith({
+      chat_id: "mockChatId",
+      protect_content: true,
+      parse_mode: FormattingOptionsTg.HTML,
+      text: expect.any(String),
+      reply_to_message_id: "mockMessageId",
+    });
+    expect(mockCallback).toHaveBeenCalledWith(null, { statusCode: OK });
+  });
+
+  it("should handle unauthorized", async () => {
+    const mockCallback = jest.fn();
+    usersDynamoService.get.mockRejectedValueOnce(new Error("mock error"));
+
+    await botnorreaCreateApiKey(
+      {
+        queryStringParameters: {
+          id: "mockUserId",
+          apiKey: "mockApiKey",
+        },
+        body: JSON.stringify(mockTelegramUpdate),
+      },
+      {},
+      mockCallback
+    );
+
+    expect(usersDynamoService.get).toHaveBeenCalledWith("mockUserId");
+    expect(usersDynamoService.getById).not.toHaveBeenCalledWith("mockUserId");
+    expect(usersDynamoService.update).not.toHaveBeenCalledWith(
+      { uuid: "mockUserUuid", apiKey: expect.any(String) },
+      true
+    );
+    expect(mockCallback).toHaveBeenCalledWith(null, {
+      statusCode: UNAUTHORIZED,
+    });
+  });
+
+  it("should handle unauthorized for missing query parameters", async () => {
+    const mockCallback = jest.fn();
+    usersDynamoService.get.mockRejectedValueOnce(new Error("mock error"));
+
+    await botnorreaCreateApiKey(
+      {
+        body: JSON.stringify(mockTelegramUpdate),
+      },
+      {},
+      mockCallback
+    );
+
+    expect(usersDynamoService.get).toHaveBeenCalledWith("undefined");
+    expect(usersDynamoService.getById).not.toHaveBeenCalledWith("mockUserId");
+    expect(usersDynamoService.update).not.toHaveBeenCalledWith(
+      { uuid: "mockUserUuid", apiKey: expect.any(String) },
+      true
+    );
+    expect(mockCallback).toHaveBeenCalledWith(null, {
+      statusCode: UNAUTHORIZED,
+    });
+  });
+
+  it("should handle bad request", async () => {
+    const mockCallback = jest.fn();
+    usersDynamoService.get.mockResolvedValueOnce(mockUser);
+    usersDynamoService.getById.mockResolvedValueOnce(mockUser);
+
+    await botnorreaCreateApiKey(
+      {
+        queryStringParameters: {
+          id: "mockUserId",
+          apiKey: "mockApiKey",
+        },
+      },
+      {},
+      mockCallback
+    );
+
+    expect(usersDynamoService.get).toHaveBeenCalledWith("mockUserId");
+    expect(usersDynamoService.getById).not.toHaveBeenCalledWith("mockUserId");
+    expect(usersDynamoService.update).not.toHaveBeenCalledWith(
+      { uuid: "mockUserUuid", apiKey: expect.any(String) },
+      true
+    );
+    expect(mockCallback).toHaveBeenCalledWith(null, {
+      statusCode: BAD_REQUEST,
+    });
+  });
+
+  it("should handle unauthorized for bad role", async () => {
+    const mockCallback = jest.fn();
+    usersDynamoService.get.mockResolvedValueOnce({
+      ...mockUser,
+      role: Role.USER,
+    });
+    usersDynamoService.getById.mockResolvedValueOnce(mockUser);
+
+    await botnorreaCreateApiKey(
+      {
+        queryStringParameters: {
+          id: "mockUserId",
+          apiKey: "mockApiKey",
+        },
+        body: JSON.stringify(mockTelegramUpdate),
+      },
+      {},
+      mockCallback
+    );
+
+    expect(usersDynamoService.get).toHaveBeenCalledWith("mockUserId");
+    expect(usersDynamoService.getById).not.toHaveBeenCalledWith("mockUserId");
+    expect(usersDynamoService.update).not.toHaveBeenCalledWith(
+      { uuid: "mockUserUuid", apiKey: expect.any(String) },
+      true
+    );
+    expect(sendMessage).not.toHaveBeenCalledWith({
+      chat_id: "mockChatId",
+      protect_content: true,
+      parse_mode: FormattingOptionsTg.HTML,
+      text: expect.any(String),
+      reply_to_message_id: "mockMessageId",
+    });
+    expect(mockCallback).toHaveBeenCalledWith(null, {
+      statusCode: UNAUTHORIZED,
+    });
+  });
+
+  it("should handle internal server error log", async () => {
+    const mockCallback = jest.fn();
+    usersDynamoService.get.mockResolvedValueOnce(mockUser);
+    usersDynamoService.getById.mockResolvedValueOnce({ uuid: "mockUserUuid" });
+    mockCallback.mockImplementationOnce(() => {
+      throw new Error("mock error");
+    });
+
+    await botnorreaCreateApiKey(
+      {
+        queryStringParameters: {
+          id: "mockUserId",
+          apiKey: "mockApiKey",
+        },
+        body: JSON.stringify(mockTelegramUpdate),
+      },
+      {},
+      mockCallback
+    );
+
+    expect(usersDynamoService.get).toHaveBeenCalledWith("mockUserId");
+    expect(usersDynamoService.getById).toHaveBeenCalledWith("mockUserId");
+    expect(usersDynamoService.update).toHaveBeenCalledWith(
+      { uuid: "mockUserUuid", apiKey: expect.any(String) },
+      true
+    );
+    expect(sendMessage).toHaveBeenCalledWith({
+      chat_id: "mockChatId",
+      protect_content: true,
+      parse_mode: FormattingOptionsTg.HTML,
+      text: expect.any(String),
+      reply_to_message_id: "mockMessageId",
+    });
+    expect(logError).toHaveBeenCalled();
+    expect(mockCallback).toHaveBeenCalledTimes(2);
   });
 });
